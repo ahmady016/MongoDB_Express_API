9a8b48664f31c454fb4e6e280be7c806b797c053
ahmady016 <ahmady09@gmail.com>
2018-07-13 13:36:58 +0200

    add config, dotenv and connecting to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-
+require('dotenv').config();
+const config = require('config')
+import mongoose from 'mongoose'
 import express from 'express'
 import cors from 'cors'
 
-const PORT = process.env.PORT || 5000;
-const server = express();
+const PORT = process.env.PORT || config.get("port");
+const app = express();
 
-server.use(cors());
+// mongoose.connect(process.env.GOOGLE_BOOKS_DB_URL)
+//         .then(() => console.log('connected to google_books db ...'))
+//         .catch(err => console.error(err.message))
 
-server.listen(PORT, () => {
+mongoose.connect(config.get("db.test"))
+        .then(() => console.log('connected to test db ...'))
+        .catch(err => console.error(err.message))
+
+app.use(cors());
+
+app.listen(PORT, () => {
   console.log(`server running at port:${PORT} \n${(new Date()).toLocaleString()}`);
 });
