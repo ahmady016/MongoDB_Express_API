0934a37e42e33193b31192322e802a2d34222b9c
ahmady016 <ahmady09@gmail.com>
2018-07-13 14:24:02 +0200

    add debug module for app logging

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config()
+const startupLog = require('debug')('app:startup')
+const dbLog = require('debug')('app:db')
 const config = require('config')
 import mongoose from 'mongoose'
 import express from 'express'
@@ -7,16 +9,17 @@ import cors from 'cors'
 const PORT = process.env.PORT || config.get("port");
 const app = express();
 
-// mongoose.connect(process.env.GOOGLE_BOOKS_DB_URL)
-//         .then(() => console.log('connected to google_books db ...'))
-//         .catch(err => console.error(err.message))
-
 mongoose.connect(config.get("db.test"))
-        .then(() => console.log('connected to test db ...'))
-        .catch(err => console.error(err.message))
+        .then(() => dbLog('connected to test db ...'))
+        .catch(err => dbLog(err.message))
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// define app routes start
 
+// define app routes end
+app.use(express.static('public'));
 app.listen(PORT, () => {
-  console.log(`server running at port:${PORT} \n${(new Date()).toLocaleString()}`);
+  startupLog(`server running on: ${config.get("address")}:${PORT} - at: ${(new Date()).toLocaleString()}`);
 });
